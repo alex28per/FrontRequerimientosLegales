7e8af8dedcf992f8337973c992d4fccd49529c13
alexisprz <alexisprz46@gmail.com>
2024-02-15 07:17:01 -0500

    cambios new

--- src/app/modules/admin/dashboards/newnorma/newnorma.component.ts
+++ src/app/modules/admin/dashboards/newnorma/newnorma.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-newnorma',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './newnorma.component.html'
 })
 export class NewnormaComponent {
+  showModal = false;
+  showModal2 = false;
+  showModal3 = false;
 
+  toggleModal(){
+    this.showModal = !this.showModal;
+  }
+
+  toggleModal2(){
+    this.showModal2 = !this.showModal2;
+  }
+
+  toggleModal3(){
+    this.showModal3 = !this.showModal3;
+  }
+
+  ngOnInit(): void{
+
+  }
+
+  
+ 
 }
